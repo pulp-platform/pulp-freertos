e5da85591d753f93c1be4ce06adb347d9b1786d2
bluew <bluewww@users.noreply.github.com>
2020-01-24 14:30:46 +0000

    Add FreeRTOS specific .clang-format and .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (projectile-project-compilation-dir . "FreeRTOS/Demo/RISC-V_PULPissimo/"))
+ (c-mode
+  (c-file-style . "bsd")
+  (tab-width . 4)
+  (indent-tabs-mode . t)))
+
+
