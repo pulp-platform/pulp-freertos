bbaa9401b72c71555219c31cc2ea0fa40022345d
bluew <bluewww@users.noreply.github.com>
2020-02-26 20:03:31 +0000

    PULPissimo: Add clint helper functions

--- FreeRTOS/Demo/RISC-V_PULPissimo/common/irq.c
+++ FreeRTOS/Demo/RISC-V_PULPissimo/common/irq.c
@@ -22,7 +22,9 @@
 #include "pulp_mem_map.h"
 #include "pulp_io.h"
 #include "irq.h"
+#include "csr.h"
 
+/* utility functions for PULPs external interrupt controller */
 void irq_mask(uint32_t mask)
 {
 	writew(mask, (uint32_t *)(PULP_FC_IRQ_ADDR + IRQ_REG_MASK_OFFSET));
@@ -37,4 +39,18 @@ void irq_disable(uint32_t mask)
 {
 	writew(mask,
 	       (uint32_t *)(PULP_FC_IRQ_ADDR + IRQ_REG_MASK_CLEAR_OFFSET));
+}
+/* utility functions for the core level interrupt (CLINT) described in the
+ * RISC-V privileged specification */
+
+uint32_t irq_clint_disable()
+{
+	uint32_t val = csr_read_clear(CSR_MSTATUS, MSTATUS_IE);
+	return val;
+}
+
+uint32_t irq_clint_enable()
+{
+	uint32_t val = csr_read_set(CSR_MSTATUS, MSTATUS_IE);
+	return val;
 }
