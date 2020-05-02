04532ec991cf1151757da2c13b566ca2c0196b5c
bluew <bluewww@users.noreply.github.com>
2020-05-02 04:31:50 +0000

    pulp/tests: Add pmsis hello world

---
+++ RISC-V_PULP_Project/tests/hello_world_pmsis/hello_world_pmsis.c
@@ -0,0 +1,126 @@
+/* Author: Robert Balas (balasr@iis.ee.ethz.ch )*/
+
+/*
+ * A minimal pmsis example. Mainly used to test if we can link against pmsis.
+ */
+
+/* FreeRTOS kernel includes. */
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* c stdlib */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <inttypes.h>
+
+/* system includes */
+#include "system_pulpissimo_ri5cy.h"
+#include "timer_irq.h"
+#include "fll.h"
+#include "irq.h"
+#include "gpio.h"
+
+/* pmsis */
+#include "pmsis.h"
+
+void vApplicationMallocFailedHook(void);
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
+void vApplicationTickHook(void);
+
+/* Prepare haredware to run the demo. */
+static void prvSetupHardware(void);
+
+char hello[40];
+
+void helloworld(void)
+{
+	printf("Entering main controller\n");
+	uint32_t core_id = pi_core_id(), cluster_id = pi_cluster_id();
+
+	sprintf(hello, "[%"PRIu32" %"PRIu32"] Hello World!\n", cluster_id, core_id);
+	puts(hello);
+
+	pmsis_exit(EXIT_SUCCESS);
+}
+
+/* Program Entry. */
+int main(void)
+{
+	/* Init board hardware. */
+	system_init();
+
+	printf("\n\n\t *** FreeRTOS HelloWorld *** \n\n");
+	return pmsis_kickoff((void *) helloworld);
+}
+
+
+void vApplicationMallocFailedHook(void)
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	taskDISABLE_INTERRUPTS();
+	printf("error: application malloc failed\n");
+	__asm volatile("ebreak");
+	for (;;)
+		;
+}
+
+void vApplicationIdleHook(void)
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
+{
+	(void)pcTaskName;
+	(void)pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	printf("error: stack overflow\n");
+	__asm volatile("ebreak");
+	for (;;)
+		;
+}
+
+void vApplicationTickHook(void)
+{
+}
+
+void vPortSetupTimerInterrupt(void)
+{
+	extern int timer_irq_init(uint32_t ticks);
+
+	/* No CLINT so use the PULP timer to generate the tick interrupt. */
+	/* TODO: configKERNEL_INTERRUPT_PRIORITY - 1 ? */
+	timer_irq_init(ARCHI_REF_CLOCK / configTICK_RATE_HZ);
+	/* TODO: allow setting interrupt priority (to super high(?)) */
+	irq_enable(IRQ_FC_EVT_TIMER0_LO);
+}
+
+void vSystemIrqHandler(uint32_t mcause)
+{
+	extern void (*isr_table[32])(void);
+	isr_table[mcause & 0xf]();
+}
