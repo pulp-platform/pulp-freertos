9226e94b04d416d5d83752245f7f9e55dd251884
bluew <bluewww@users.noreply.github.com>
2020-05-02 04:29:01 +0000

    pulp/nortos: Add memory scan test
    Tests that fills the interleaved bank with a fixed pattern (incrementing
    integers). Thiis is used to test if we can
    1. write to the l2 address space without failure
    2. debug tcl scripts that generate a flat memory dump (see mem.tcl in pulp)

---
+++ RISC-V_PULP_Project/nortos/memscan/memscan.c
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Author: Robert Balas (balasr@iis.ee.ethz.ch)
+ * Description: Write to the whole l2 address space
+ * (skipping the two private banks)
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "pulp_io.h"
+
+int main(int argc, char *argv[])
+{
+	uint32_t val = 0;
+	printf("memscan start\n");
+	for (uint32_t addr = 0x1c020000; addr < 0x1c080000; addr += 4) {
+		val += 1;
+		writew(val, addr);
+	}
+	printf("memscan finished\n");
+	return EXIT_SUCCESS;
+}
