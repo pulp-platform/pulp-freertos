574fde3ee296ab65dc8f2f5ea56ec03c7f65f8b9
bluew <bluewww@users.noreply.github.com>
2020-05-22 01:00:06 +0000

    pulp: Add support tooling for DPI
    * support.mk: make dependency information
    * dpi-models: peripheral DPI models for sim
    * pulp-debug-bridge: dependency
    * json-tools: dependency

---
+++ RISC-V_PULP_Project/support/README.md
@@ -0,0 +1,8 @@
+# Support
+Supporting libraries for simulation and more.
+
+## Overview
++ json-tools: parsing json files
++ dpi-models: SystemVerilog DPI models for peripherals. Used in tests.
++ pulp-debug-bridge: GDB to Hardware bridge. Translates GDB command into a
+  language older RI5CY cores understand.
---
+++ RISC-V_PULP_Project/support/support.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: Apache-2.0
+# Author: Robert Balas (balasr@iis.ee.ethz.ch)
+# Try to build a full dependency tree for all libraries
+
+SUPPORT_LIB_DIR = $(SUPPORT_ROOT)/install/lib
+
+$(SUPPORT_LIB_DIR)/libjson.a: \
+$(shell find "$(SUPPORT_ROOT)/json-tools" -name '*.cpp' -o -name '*.hpp')
+	INSTALL_DIR="$(SUPPORT_ROOT)/install" make -C "$(SUPPORT_ROOT)/json-tools" all install
+
+$(SUPPORT_LIB_DIR)/libpulpdebugbridge.so: \
+$(shell find "$(SUPPORT_ROOT)/pulp-debug-bridge" -name '*.cpp' -o -name '*.hpp')
+	INSTALL_DIR="$(SUPPORT_ROOT)/install" make -C "$(SUPPORT_ROOT)/pulp-debug-bridge" build
+
+$(SUPPORT_LIB_DIR)/libpulpperiph.so $(SUPPORT_LIB_DIR)/libpulpdpi.so &: \
+$(SUPPORT_LIB_DIR)/libjson.a $(SUPPORT_LIB_DIR)/libpulpdebugbridge.so \
+$(shell find "$(SUPPORT_ROOT)/dpi-models" -name '*.cpp' -o -name '*.hpp')
+	INSTALL_DIR="$(SUPPORT_ROOT)/install" make -C "$(SUPPORT_ROOT)/dpi-models" build
+
+# we need this hack to make vsim find the dpi libraries
+export LD_LIBRARY_PATH += $(SUPPORT_LIB_DIR)
