478aa1fb0cb2e401e9197cc3a4cf0f82c7db08a6
bluew <bluewww@users.noreply.github.com>
2020-06-05 21:54:04 +0000

    pulp/nortos/memscan: Fix range

--- RISC-V_PULP_Project/nortos/memscan/memscan.c
+++ RISC-V_PULP_Project/nortos/memscan/memscan.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	uint32_t val = 0;
 	printf("memscan start\n");
-	for (uint32_t addr = 0x1c020000; addr < 0x1c080000; addr += 4) {
+	for (uint32_t addr = 0x1c010000; addr < 0x1c080000; addr += 4) {
 		val += 1;
 		writew(val, addr);
 	}
