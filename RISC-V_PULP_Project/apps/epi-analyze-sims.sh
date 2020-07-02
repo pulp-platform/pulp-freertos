00cadc51b7ee42a74c2a1cb0f80415bcc70c8b66
bluew <bluewww@users.noreply.github.com>
2020-07-02 18:27:10 +0200

    pulp/apps/scripts: Update
    * Names
    * Block until forked processes finish
    * Make opt a parameter to be passed

--- RISC-V_PULP_Project/apps/epi-analyze-sims.sh
+++ RISC-V_PULP_Project/apps/epi-analyze-sims.sh
@@ -22,19 +22,43 @@ usage () {
 # opt1_dir_period_200_iter_20 opt1_dir_period_20_iter_20 opt1_dir_period_250_iter_20 opt1_dir_period_300_iter_20 \
 # opt1_dir_period_30_iter_20 opt1_dir_period_350_iter_20 opt1_dir_period_400_iter_20 opt1_dir_period_40_iter_20 \
 # opt1_dir_period_450_iter_20 opt1_dir_period_500_iter_20 opt1_dir_period_50_iter_20)
-dirs=(opt1_dir_period_8_iter_20 opt1_dir_period_7_iter_20 opt1_dir_period_6_iter_20 opt1_dir_period_5_iter_20)
+# dirs=(opt1_dir_period_8_iter_20 opt1_dir_period_7_iter_20 opt1_dir_period_6_iter_20 opt1_dir_period_5_iter_20)
 
+
+dirs=(opt1_period_100_iter_20 \
+opt1_period_10_iter_20 \
+opt1_period_150_iter_20 \
+opt1_period_200_iter_20 \
+opt1_period_20_iter_20 \
+opt1_period_250_iter_20 \
+opt1_period_300_iter_20 \
+opt1_period_30_iter_20 \
+opt1_period_350_iter_20 \
+opt1_period_400_iter_20 \
+opt1_period_40_iter_20 \
+opt1_period_450_iter_20 \
+opt1_period_500_iter_20 \
+opt1_period_50_iter_20 \
+opt1_period_5_iter_20 \
+vanilla_period_100_iter_20 \
+vanilla_period_10_iter_20 \
+vanilla_period_150_iter_20 \
+vanilla_period_200_iter_20 \
+vanilla_period_20_iter_20 \
+vanilla_period_250_iter_20 \
+vanilla_period_300_iter_20 \
+vanilla_period_30_iter_20 \
+vanilla_period_350_iter_20 \
+vanilla_period_400_iter_20 \
+vanilla_period_40_iter_20 \
+vanilla_period_450_iter_20 \
+vanilla_period_500_iter_20 \
+vanilla_period_50_iter_20 \
+vanilla_period_5_iter_20)
 
 for f in "${dirs[@]}"; do
     printf "\nANALYZING and FORKING $f\n"
-    ./epi-analyze-perf.sh "/scratch/balasr/sim/2020-06-11/$f" "/scratch/balasr/sim/2020-06-11/stats/$f" &
+    ./epi-analyze-perf.sh "/scratch/balasr/sim/2020-06-26/$f" "/scratch/balasr/sim/2020-06-26/stats/$f" &
 done
-# those are broken
-#vanilla_dir_period_5_iter_20
-#vanilla_dir_period_10_iter_20
-#vanilla_dir_period_1_iter_20
-#vanilla_dir_period_40_iter_20
 
-# those are broken
-#opt1_dir_period_1_iter_20
-#opt1_dir_period_5_iter_20
+wait < <(jobs -p)
--- RISC-V_PULP_Project/apps/epi-run-sims.sh
+++ RISC-V_PULP_Project/apps/epi-run-sims.sh
@@ -2,20 +2,41 @@
 
 set -e
 
+usage () {
+    echo "Usage: $0 [-o] simdir"
+    echo "-o: Run with optimizations"
+    exit
+}
+
+opt=no
+while getopts ":o" o; do
+    case "${o}" in
+        o)
+            opt=yes
+            ;;
+        *)
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+[[ -d $1 ]] || usage
+
 # compiler
 export RISCV=$HOME/.riscv32e
 # pulp compiled rtl simulation
 source $HOME/projects/pulp/setup/vsim.sh
 
 epi_dir=$(realpath epi_power/Code)
-sim_dir_prefix=/scratch/balasr/sim/2020-06-11
-sim_dir="$sim_dir_prefix/vanilla_dir"
+sim_dir="/scratch/balasr/sim/0000-00-00/vanilla_dir"
+sim_dir="$1"
 
 # from EPI_Config.h
 # define MEASURE_N_ITERATION 20 // This is needed for both debug and Measure (DEFAULT)
 measure_iterations=20
 # define TASKS_CONTROL_PERIOD_US (500) // Time in us //when change this also hange TELEMETRY_POLLING_FREQUENCY
-periods=(500 450 400 350 300 250 200 150 100 50 40 30 20 10 5 1)
+periods=(500 450 400 350 300 250 200 150 100 50 40 30 20 10 5)
+
 
 for p in "${periods[@]}"; do
     m=$measure_iterations
@@ -23,7 +44,7 @@ for p in "${periods[@]}"; do
     # TODO: opt level
     cd "$epi_dir"
     echo "compiling firmware"
-    make clean all CPPEXTRA="-DMEASURE_N_ITERATION=${m} -DTASKS_CONTROL_PERIOD_US=${p}"
+    make clean all "opt1=$opt" CPPEXTRA="-DMEASURE_N_ITERATION=${m} -DTASKS_CONTROL_PERIOD_US=${p}"
 
     # define simulation directory
     this_simdir="${sim_dir}_period_${p}_iter_${m}"
