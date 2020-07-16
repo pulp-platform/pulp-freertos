8b3c3925a65594fd2e791e87ac3cdb0c06a39894
bluew <bluewww@users.noreply.github.com>
2020-07-16 20:20:54 +0200

    Add hello world template
    This is the starting point for anyone that wants to make a new project.

---
+++ template/hello_world/helloworld.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Author: Robert Balas (balasr@iis.ee.ethz.ch) */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello world!\n");
+	return EXIT_SUCCESS;
+}
