7cf6c3504133a06ffbd0fbac264852f0e458fd9e
bluew <bluewww@users.noreply.github.com>
2020-07-16 17:29:47 +0200

    Remove FreeRTOS demos

--- FreeRTOS/Demo/RISC-V_PULPissimo/scripts/bindiff.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-usage () {
-    echo "Usage: bindiff elf-a elf-b"
-    exit
-}
-[[ -n $RISCV ]] || { echo "RISCV environment variable unset. Please point it to your toolchain."; exit 1; }
-[[ -f $1 ]] || usage
-[[ -f $2 ]] || usage
-scripts/elf_diff/bin/elf_diff --bin_dir "$RISCV/bin" --bin-prefix "riscv32-unknown-elf-" $1 $2
--- FreeRTOS/Demo/RISC-V_PULPissimo/scripts/install-elf-diff.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-[[ ! -d elf_diff ]] || { echo "elf_diff is already downloaded. Aborting."; exit 1; }
-
-git clone git@github.com:bluewww/elf_diff.git
-patch -p1 -d elf_diff/ < elf-diff-py3.patch
