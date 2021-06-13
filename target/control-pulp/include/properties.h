72139862b2c9cf95d7792805e5e86011714b5ae5
bluew <bluewww@users.noreply.github.com>
2021-06-13 03:32:46 +0200

    control-pulp: Fix properties

--- target/control-pulp/include/properties.h
+++ target/control-pulp/include/properties.h
@@ -31,7 +31,7 @@
 #define ARCHI_REF_CLOCK_LOG2 15
 #define ARCHI_REF_CLOCK	     (1 << ARCHI_REF_CLOCK_LOG2)
 
-#define ARCHI_NB_FLL 3
+#define ARCHI_NB_FLL 0
 
 #define __RT_FLL_CL	2
 #define __RT_FLL_PERIPH 1
@@ -76,50 +76,33 @@
 #define UDMA_HAS_UART	(1)
 #define UDMA_HAS_I2C	(1)
 #define UDMA_HAS_DMACPY (0)
-#define UDMA_HAS_I2S	(1)
-#define UDMA_HAS_CPI	(1)
+#define UDMA_HAS_I2S	(0)
+#define UDMA_HAS_CPI	(0)
 
 /* TODO: fix this table */
 /* Number of UDMA peripherals */
-#define UDMA_NB_SPIM   (2)
+#define UDMA_NB_SPIM   (8)
 #define UDMA_NB_HYPER  (0)
 #define UDMA_NB_UART   (1)
-#define UDMA_NB_I2C    (2)
+#define UDMA_NB_I2C    (12)
+/* TODO: i2c slaves */
 #define UDMA_NB_DMACPY (0)
-#define UDMA_NB_I2S    (1)
-#define UDMA_NB_CPI    (1)
+#define UDMA_NB_I2S    (0)
+#define UDMA_NB_CPI    (0)
 
-/* TODO: fix this table */
-/* #define UDMA_NB_PERIPH              ((UDMA_HAS_SPIM ? UDMA_NB_SPIM) + \ */
-/*                                      (UDMA_HAS_HYPER ? UDMA_NB_HYPER) + \ */
-/*                                      (UDMA_HAS_UART ? UDMA_NB_UART) + \ */
-/*                                      (UDMA_HAS_I2C ? UDMA_NB_I2C) + \ */
-/*                                      (UDMA_HAS_DMACPY ? UDMA_NB_DMACPY) + \
- */
-/*                                      (UDMA_HAS_I2S ? UDMA_NB_I2S) + \ */
-/*                                      (UDMA_HAS_CPI ? UDMA_NB_CPI)) */
-#define UDMA_NB_PERIPH (10)
+#define UDMA_NB_PERIPH (26)
 /* Size of each UDMA peripheral */
 #define UDMA_PERIPH_SIZE_LOG2 (7)
 #define UDMA_PERIPH_SIZE      (1 << UDMA_PERIPH_SIZE_LOG2)
 
 /* UDMA peripherals ID, this maps to PER_ID_* in udma_subsystem.sv */
-#define UDMA_SPIM_ID(id) (1 + (id))
-/* #define UDMA_HYPER_ID(id)           (3   + (id)) */
 #define UDMA_UART_ID(id) (0 + (id))
-#define UDMA_I2C_ID(id)	 (2 + (id))
-/* #define UDMA_DMACPY_ID(id)          (7   + (id)) */
-#define ARCHI_UDMA_FILTER_ID(id) (7 + (id))
-#define UDMA_I2S_ID(id)		 (5 + (id))
-#define UDMA_CPI_ID(id)		 (6 + (id))
-#define UDMA_SDIO_ID(id)	 (4 + (id))
+#define UDMA_SPIM_ID(id) (1 + (id))
+#define UDMA_I2C_ID(id)	 (9 + (id))
 
 
 /* Pads & GPIO. */
-#define ARCHI_NB_PAD  (48)
-#define ARCHI_NB_GPIO (32)
-
-/* PWM. */
-/* #define ARCHI_NB_PWM                (4) */
+#define ARCHI_NB_PAD  (0)
+#define ARCHI_NB_GPIO (128)
 
 #endif /* __PROPERTIES_H__ */
