d08bf61e2d29780b26ee005fb89dcb94f187c14a
bluew <bluewww@users.noreply.github.com>
2021-10-22 02:59:21 +0200

    drivers/uart: Add missing register access macros

--- drivers/include/uart_periph.h
+++ drivers/include/uart_periph.h
@@ -65,6 +65,19 @@ typedef struct {
 #define UART_SETUP_STOP_BITS_MASK  (0x8)
 #define UART_SETUP_STOP_BITS_SHIFT (3)
 
+/* Set polling mode:
+  - 1'b0: disable
+  - 1'b1: Enable polling data transfer. Wait until valid register signals ok,
+    then read data register. Valid register automatically cleared after read.*/
+#define UART_SETUP_POLLING_EN_MASK  (0x10)
+#define UART_SETUP_POLLING_EN_SHIFT (4)
+
+/* Set clean fifo mode:
+  - 1'b0: disable
+  - 1'b1: clean the rx fifo */
+#define UART_SETUP_CLEAN_FIFO_MASK  (0x20)
+#define UART_SETUP_CLEAN_FIFO_SHIFT (5)
+
 /* Set TX transceiver state:
   - 1'b0: disable
   - 1'b1: enable */
@@ -80,6 +93,46 @@ typedef struct {
 /* Sets the clock divider ratio for the baud rate generator. */
 #define UART_SETUP_CLKDIV_MASK	(0xffff0000)
 #define UART_SETUP_CLKDIV_SHIFT (16)
+
+/*! @name ERROR register */
+
+/* Reports overflow error:
+  - 1'b0: no error
+  - 1'b1: rx overflow error */
+#define UART_ERROR_RX_ERR_OVERFLOW_MASK	 (0x1)
+#define UART_ERROR_RX_ERR_OVERFLOW_SHIFT (0)
+
+/* Reports parity error:
+  - 1'b0: no error
+  - 1'b1: rx parity error */
+#define UART_ERROR_RX_ERR_PARITY_MASK  (0x2)
+#define UART_ERROR_RX_ERR_PARITY_SHIFT (1)
+
+/*! @name IRQ_EN register */
+
+/* Rx interrupt in enable flag:
+  - 1’b0: RX IRQ disable
+  - 1’b1: RX IRQ enable */
+#define UART_IRQ_EN_RX_MASK  (0x1)
+#define UART_IRQ_EN_RX_SHIFT (0)
+
+/* Error interrupt in enable flag:
+  - 1’b0: Error IRQ disable
+  - 1’b1: Error IRQ enable*/
+#define UART_IRQ_EN_ERROR_MASK	(0x2)
+#define UART_IRQ_EN_ERROR_SHIFT (1)
+
+/*! @name READY register */
+
+/* Used only in RX polling method to indicate data is ready for read:
+  - 1’b0: Data is not ready to read
+  - 1’b1: Data is ready to read */
+#define UART_VALID_READY_MASK  (0x1)
+#define UART_VALID_READY_SHIFT (0)
+
+/* RX read data for polling or interrupt */
+#define UART_DATA_MASK	(0xff)
+#define UART_DATA_SHIFT (0)
 
 
 #define uart(id) ((uart_t *)UDMA_UART(id))
